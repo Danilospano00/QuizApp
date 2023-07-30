f43fab0042b03ec6586cf2cfcfac0ed4e2bca2a3
d.spano <d.spano@young.business>
2023-07-30 19:01:02 +0200

    all

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val etName: AppCompatEditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+            if (etName.text?.isEmpty() == true) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
